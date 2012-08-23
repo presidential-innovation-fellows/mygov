7e4bdd25e756698f0d2db6445f2c235852f4ce48
Benjamin J. Balter <ben@balter.com>
2012-08-23 00:01:18 -0400

    fix for committer url pointing to api

--- _includes/js/commits.js
+++ _includes/js/commits.js
@@ -5,7 +5,8 @@ jQuery(document).ready(function($){
 		$.each( $(data.data).slice(0,5), function(k,v) {
 			var url = v.url;
 			url = url.replace( 'https://api.github.com/repos/{{ site.github.repo }}/commits/', 'https://github.com/{{ site.github.repo }}/commit/' );
-			$('#commits').append( '<li><a href="' + url + '">' + v.commit.message + '</a> by <a href="' + v.committer.url + '">' + v.commit.author.name + '</a> - ' + $.relatizeDate.timeAgoInWords( new Date( v.commit.author.date ) ) + '</li>');
+			committer_url = v.committer.url.replace( 'https://api.github.com/users/', 'https://github.com/' );
+			$('#commits').append( '<li><a href="' + url + '">' + v.commit.message + '</a> by <a href="' + committer_url + '">' + v.commit.author.name + '</a> - ' + $.relatizeDate.timeAgoInWords( new Date( v.commit.author.date ) ) + '</li>');
 				
 		});
 
